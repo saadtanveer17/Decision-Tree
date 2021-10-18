f0e88351ebce721a26c0ffeec70bfb24b4051ac5
saadtanveer17 <saadtanveer17@gmail.com>
2021-10-18 21:14:35 +0500

    Added csv export method

--- db.py
+++ db.py
@@ -1,12 +1,22 @@
 import json
+import pandas 
 import pymongo
-import pandas as pd
 from bson.json_util import loads, dumps
 
 
 def connect_to_db(db_address):
     # Connecting to Database server
     return pymongo.MongoClient(db_address)
+
+def export_dataframe(client, db_name, collection_name, csv_file_name):
+    # Accessing db
+    db = client[db_name]
+
+    # Accessing collection
+    collection = db[collection_name]
+    df = pandas.read_csv(csv_file_name)
+    df_json = df.to_dict('records') 
+    collection.insert_many(df_json)
 
 def import_dataframe(client, db_name, collection_name):
     # Accessing db
@@ -24,14 +34,16 @@ def import_dataframe(client, db_name, collection_name):
     # Converting to the JSON
     json_data = dumps(list_cur, indent = 2) 
 
-    print(json_data)
-    df = pd.read_json(json_data)
-    print(df)
+    # print(json_data)
+    df = pandas.read_json(json_data)
+    # print(df)
     return df
 
 
 db_address = 'mongodb://mongodb:mongodb@172.22.0.3:27017/'
 db_name = 'demo_db'
+csv_file_name = 'shows.csv'
 collection_name = 'demo_data'
 client = connect_to_db(db_address)
+# export_dataframe(client, db_name, collection_name, csv_file_name)
 import_dataframe(client, db_name, collection_name)
--- demo.py
+++ demo.py
@@ -4,19 +4,17 @@ import pydotplus
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 import matplotlib.image as pltimg
-
-df = pandas.read_csv("shows.csv")
-
-for i, j in df.iterrows():
-    print(j)
-    print()
-
-df['json'] = df.apply(lambda x: x.to_json(), axis=1)
-
-for i in df['json']:
-    print(i)
+import json
+import pymongo
+from bson.json_util import loads, dumps
+import db
 
 
+db_address = 'mongodb://mongodb:mongodb@172.22.0.3:27017/'
+db_name = 'demo_db'
+collection_name = 'demo_data'
+client = db.connect_to_db(db_address)
+df = db.import_dataframe(client, db_name, collection_name)
 
 d = {'UK': 0, 'USA': 1, 'N': 2}
 df['Nationality'] = df['Nationality'].map(d)
@@ -28,12 +26,13 @@ features = ['Age', 'Experience', 'Rank', 'Nationality']
 X = df[features]
 y = df['Go']
 
-# dtree = DecisionTreeClassifier()
-# dtree = dtree.fit(X, y)
-# data = tree.export_graphviz(dtree, out_file=None, feature_names=features)
-# graph = pydotplus.graph_from_dot_data(data)
-# graph.write_png('mydecisiontree.png')
+print(X, y)
+dtree = DecisionTreeClassifier()
+dtree = dtree.fit(X, y)
+data = tree.export_graphviz(dtree, out_file=None, feature_names=features)
+graph = pydotplus.graph_from_dot_data(data)
+graph.write_png('mydecisiontree.png')
 
-# img=pltimg.imread('mydecisiontree.png')
-# imgplot = plt.imshow(img)
+img=pltimg.imread('mydecisiontree.png')
+imgplot = plt.imshow(img)
 # plt.show()
