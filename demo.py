2c46c11c64f2c43d784b185650a0d723eca00b6d
saadtanveer17 <saadtanveer17@gmail.com>
2021-10-19 22:22:47 +0500

    Added dynamic data hashing of dataframes

--- demo.py
+++ demo.py
@@ -9,19 +9,37 @@ import pymongo
 from bson.json_util import loads, dumps
 import db
 
-
 db_address = 'mongodb://mongodb:mongodb@172.22.0.3:27017/'
 db_name = 'demo_db'
 collection_name = 'demo_data'
 client = db.connect_to_db(db_address)
 df = db.import_dataframe(client, db_name, collection_name)
 
-d = {'UK': 0, 'USA': 1, 'N': 2}
-df['Nationality'] = df['Nationality'].map(d)
-d = {'YES': 1, 'NO': 0}
-df['Go'] = df['Go'].map(d)
+print(df)
+for column in df:
+    if (column != '_id') and (df[column].dtypes == object):
+        yo = {}
+        print("NAME#", column)
+        df_column = df[column].drop_duplicates()
+        df_np = df_column.to_numpy()
+        for i in range(len(df_np)):
+            yo[df_np[i]] = i
+        df[column] = df[column].map(yo)
+        print(yo)
+        print()
+        print(df)
+        print()
 
-features = ['Age', 'Experience', 'Rank', 'Nationality']
+
+# d = {'UK': 0, 'USA': 1, 'N': 2}
+# df['Nationality'] = df['Nationality'].map(d)
+# d = {'YES': 1, 'NO': 0}
+# df['Go'] = df['Go'].map(d)
+# features = ['Age', 'Experience', 'Rank', 'Nationality']
+
+df_np = df.columns.to_numpy()
+features = df_np[1:-1]
+print(features)
 
 X = df[features]
 y = df['Go']
