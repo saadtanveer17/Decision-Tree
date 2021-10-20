17cdf89c1f1b779fbcb8c0846cf8ba5bc6d96596
saadtanveer17 <saadtanveer17@gmail.com>
2021-10-20 06:32:28 +0500

    Removed useless code

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ import matplotlib.image as pltimg
 from sklearn.tree import DecisionTreeClassifier
 from db import DB as Database
 
-
+# Uncomment print statements to observe data evolution
 class SmartDecisionTreeClassifier(object):
     
     def __init__(self, dataframe):
@@ -33,13 +33,14 @@ class SmartDecisionTreeClassifier(object):
 
     def prepare_data(self):
         self.X = self.df[self.features]
-        self.Y = self.df[self.df.columns[-1]]      
-        # print(self.X, self.Y)
+        self.Y = self.df[self.df.columns[-1]]
+        # print('\nX-axes\n', self.X, '\nY-axes\n', self.Y)
 
     def apply_decision_tree(self):
         dtree = DecisionTreeClassifier()
         dtree = dtree.fit(self.X, self.Y)
         self.dtree_data = tree.export_graphviz(dtree, out_file=None, feature_names=self.features)
+        # print(self.dtree_data)
         
     def plot_graph(self):
         self.graph = pydotplus.graph_from_dot_data(self.dtree_data)
@@ -72,55 +73,3 @@ if __name__ == '__main__':
     # print(df)
     dt_obj = SmartDecisionTreeClassifier(df)
     dt_obj.process_df()
-
-
-# # db_address = 'mongodb://mongodb:mongodb@172.22.0.3:27017/'
-# db_address = 'mongodb://mongodb:mongodb@172.22.0.2:27017/'
-# db_name = 'demo_db'
-# collection_name = 'demo_data'
-# csv_file_name = 'shows.csv'
-# client = db.connect_to_db(db_address)
-# # db.export_dataframe(client, db_name, collection_name, csv_file_name)
-# df = db.import_dataframe(client, db_name, collection_name)
-
-# print(df)
-# for column in df:
-#     if (column != '_id') and (df[column].dtypes == object):
-#         yo = {}
-#         print("NAME#", column)
-#         df_column = df[column].drop_duplicates()
-#         df_np = df_column.to_numpy()
-#         for i in range(len(df_np)):
-#             yo[df_np[i]] = i
-#         df[column] = df[column].map(yo)
-#         print(yo)
-#         print()
-#         print(df)
-#         print()
-
-
-# # d = {'UK': 0, 'USA': 1, 'N': 2}
-# # df['Nationality'] = df['Nationality'].map(d)
-# # d = {'YES': 1, 'NO': 0}
-# # df['Go'] = df['Go'].map(d)
-# # features = ['Age', 'Experience', 'Rank', 'Nationality']
-
-
-
-# df_np = df.columns.to_numpy()
-# features = df_np[1:-1]
-# print(features)
-
-# X = df[features]
-# y = df['Go']
-
-# print(X, y)
-# dtree = DecisionTreeClassifier()
-# dtree = dtree.fit(X, y)
-# data = tree.export_graphviz(dtree, out_file=None, feature_names=features)
-# graph = pydotplus.graph_from_dot_data(data)
-# graph.write_png('mydecisiontree.png')
-
-# img=pltimg.imread('mydecisiontree.png')
-# imgplot = plt.imshow(img)
-# plt.show()
